d2d7ffeb20621097996ee1df46f5004751a99e2b
Eddy Castillo <dyskette@gmail.com>
2025-02-18 21:06:52 -0600

    Update config

--- bash/bashrc.d/bat.sh
+++ bash/bashrc.d/bat.sh
@@ -1,5 +1,5 @@
 if which gsettings &>/dev/null && [ "$(gsettings get org.gnome.desktop.interface color-scheme)" == \'prefer-dark\' ]; then
-    export BAT_THEME=kanagawa-wave
+    export BAT_THEME=everforest
 else
-    export BAT_THEME=kanagawa-lotus
+    export BAT_THEME=rose-pine-dawn
 fi
