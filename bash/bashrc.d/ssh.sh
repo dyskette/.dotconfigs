bbb5e52c06fe2c5a05cdf101d3da9ffc3566b014
Eddy Castillo <dyskette@gmail.com>
2025-06-26 14:51:13 -0600

    Fix hardcoded ssh key

--- bash/bashrc.d/ssh.sh
+++ bash/bashrc.d/ssh.sh
@@ -1,7 +1,12 @@
 if [ "$(systemd-detect-virt)" == "wsl" ] && ! [ $(ps ax | grep [s]sh-agent | wc -l) -gt 0 ]; then
     eval $(ssh-agent -s)
     if [ "$(ssh-add -l)" == "The agent has no identities." ] ; then
-        ssh-add ~/.ssh/id_rsa
+        for pubkey in ~/.ssh/*.pub; do
+            privkey="${pubkey%.pub}"
+            if [[ -f "$privkey" ]]; then
+                ssh-add "$privkey" 2>/dev/null
+            fi
+        done
     fi
 
     # Don't leave extra agents around: kill it on exit. You may not want this part.
