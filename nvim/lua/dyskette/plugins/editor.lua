f1b06000d82077d9f8a2dfa28a387b3e3e425963
Eddy Castillo <dyskette@gmail.com>
2025-07-18 16:09:31 -0600

    feat(neovim): Add live rename

--- nvim/lua/dyskette/plugins/editor.lua
+++ nvim/lua/dyskette/plugins/editor.lua
@@ -30,6 +30,10 @@ local nvim_highlight_colors_config = function()
 	require("nvim-highlight-colors").setup({})
 end
 
+local live_rename_config = function()
+	require("live-rename").setup({})
+end
+
 return {
 	-- Detect expandtab, tabstop, softtabstop and shiftwidth automatically
 	{
@@ -56,6 +60,7 @@ return {
 		event = utils.events.InsertEnter,
 		config = autotag_config,
 	},
+	{ "saecki/live-rename.nvim", event = utils.events.VeryLazy, config = live_rename_config },
 	-- Code commenting with vim motions
 	{
 		"numToStr/Comment.nvim",
