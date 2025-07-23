80db9cb855632c9c62459ce499354cf50563938a
Eddy Castillo <dyskette@gmail.com>
2025-07-22 20:29:00 -0600

    feat(nvim): Add windsurf, update plugins, remove bat from fzf

--- nvim/lua/dyskette/plugins/fzf.lua
+++ nvim/lua/dyskette/plugins/fzf.lua
@@ -1,27 +1,30 @@
 local utils = require("dyskette.utils")
 
 local fzf_lua_config = function()
-	local fzf = require("fzf-lua")
+  local fzf = require("fzf-lua")
 
-	fzf.setup({
-		winopts = { preview = { default = "bat" } },
-		oldfiles = { cwd_only = true, include_current_session = true },
-		files = {
-			actions = { ["ctrl-q"] = { fn = fzf.actions.file_sel_to_qf, prefix = "select-all" } },
-		},
-		grep = {
-			actions = { ["ctrl-q"] = { fn = fzf.actions.file_sel_to_qf, prefix = "select-all" } },
-		},
-	})
+  fzf.setup({
+    fzf_opts = {
+      ["--cycle"] = true,
+    },
+    -- winopts = { preview = { default = "bat" } },
+    oldfiles = { cwd_only = true, include_current_session = true },
+    files = {
+      actions = { ["ctrl-q"] = { fn = fzf.actions.file_sel_to_qf, prefix = "select-all" } },
+    },
+    grep = {
+      actions = { ["ctrl-q"] = { fn = fzf.actions.file_sel_to_qf, prefix = "select-all" } },
+    },
+  })
 
-	fzf.register_ui_select()
+  fzf.register_ui_select()
 
-	require("dyskette.keymaps").fzf()
+  require("dyskette.keymaps").fzf()
 end
 
 return {
-	"ibhagwan/fzf-lua",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = fzf_lua_config,
-	event = utils.events.VeryLazy,
+  "ibhagwan/fzf-lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = fzf_lua_config,
+  event = utils.events.VeryLazy,
 }
