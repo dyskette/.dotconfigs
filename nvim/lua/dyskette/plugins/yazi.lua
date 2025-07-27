83caba3ae9274da0a257dafb649d5d014a79fa1a
Eddy Castillo <dyskette@gmail.com>
2025-07-27 12:34:05 -0600

    feat(vscode): Add vim-like keybindings for terminal and git

--- nvim/lua/dyskette/plugins/yazi.lua
+++ nvim/lua/dyskette/plugins/yazi.lua
@@ -1,25 +1,36 @@
 local utils = require("dyskette.utils")
 
 local yazi_config = function()
-	require("yazi").setup({
-		open_for_directories = true,
-		keymaps = {
-			show_help = "<f1>",
-		},
-	})
+  require("yazi").setup({
+    open_for_directories = true,
+    keymaps = {
+      show_help = "<f1>",
+    },
+  })
 
-	require("dyskette.keymaps").yazi()
+  require("dyskette.keymaps").yazi()
+end
+
+local oil_config = function()
+  require("oil").setup()
+  vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 end
 
 return {
-	"mikavilpas/yazi.nvim",
-	event = utils.events.VeryLazy,
-	keys = {
-		{ "<leader>e", mode = { "n", "v" } },
-	},
-	config = yazi_config,
-	dependencies = {
-		{ "nvim-lua/plenary.nvim", lazy = true },
-		{ "folke/snacks.nvim", lazy = true },
-	},
+  {
+    "stevearc/oil.nvim",
+    config = oil_config,
+  },
+  {
+    "mikavilpas/yazi.nvim",
+    event = utils.events.VeryLazy,
+    keys = {
+      { "<leader>e", mode = { "n", "v" } },
+    },
+    config = yazi_config,
+    dependencies = {
+      { "nvim-lua/plenary.nvim", lazy = true },
+      { "folke/snacks.nvim", lazy = true },
+    },
+  },
 }
