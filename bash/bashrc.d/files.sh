9d9a4affb5904911721532780c061f8593e06f74
Eddy Castillo <dyskette@gmail.com>
2025-09-04 20:22:51 -0600

    feat(bash): Add sg to grep files in bash

--- bash/bashrc.d/files.sh
+++ bash/bashrc.d/files.sh
@@ -21,3 +21,29 @@ function sf() {
       --preview 'bat --color=always --style=plain {}')" \
   && nvim $file_path
 }
+
+function sg() {
+  local search_query="$*"
+  
+  if [ -z "$search_query" ]; then
+    search_query=""
+  fi
+  
+  local exec_ripgrep='rg --column --color=always --smart-case {q} || :'
+  local exec_nvim='nvim {1} +{2}'
+  local exec_bat='bat --style=numbers --color=always --highlight-line {2} {1}'
+  
+  fzf \
+    --disabled \
+    --ansi \
+    --bind "start:reload:$exec_ripgrep" \
+    --bind "change:reload:$exec_ripgrep" \
+    --bind "enter:become:$exec_nvim" \
+    --layout reverse \
+    --height 50% \
+    --min-height 20 \
+    --delimiter : \
+    --preview "$exec_bat" \
+    --preview-window "+{2}/2" \
+    --query "$search_query"
+}
