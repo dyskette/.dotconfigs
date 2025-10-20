666a3e0a7a0e7990a9bfd839725c190e02ab2586
Eddy Castillo <dyskette@gmail.com>
2025-10-20 10:02:22 -0600

    feat(nvim): Change trouble preview style

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,6 +1,13 @@
 return {
   "folke/trouble.nvim",
-  opts = {},
+  opts = {
+    ---@type trouble.Window.opts
+    preview = {
+      type = "split",
+      relative = "win",
+      position = "top",
+    },
+  },
   cmd = "Trouble",
   keys = require("config.keymaps").trouble,
 }
